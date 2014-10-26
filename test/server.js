e5d2beec2a5da4ebfbaffe2209d220d3f23fff90
Dmitry Chleck <dmitrychleck@gmail.com>
2014-10-26 15:03:32 +0400

    test(Server): improve test coverage

--- test/server.js
+++ test/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// var request = require('supertest');
+var http = require('http');
+var https = require('https');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 var Rama = require('..');
 
@@ -19,6 +21,32 @@ describe('Rama', function() {
     it('should add HTTPS server if cert and key is defined', function() {
       var rama = new Rama(function() {} );
       rama.listen('localhost', 9082, './cert/cert.pem', './cert/key.pem');
+    });
+  });
+  describe('.run()', function() {
+    it('should run HTTP server', function(done) {
+      var rama = new Rama(function(req, res) {
+        res.end();
+      });
+      rama.listen('localhost', 9081).run();
+      http.get('http://localhost:9081', function(res) {
+        res.statusCode.should.eql(200);
+        done();
+      }).on('error', function(e) {
+        throw e;
+      });
+    });
+    it('should run HTTPS server', function(done) {
+      var rama = new Rama(function(req, res) {
+        res.end();
+      });
+      rama.listen('localhost', 9082, __dirname + '/files/cert.pem', __dirname + '/files/key.pem').run();
+      https.get('https://localhost:9082', function(res) {
+        res.statusCode.should.eql(200);
+        done();
+      }).on('error', function(e) {
+        throw e;
+      });
     });
   });
 });
